515e650aa11cd167d26bf8f2a77cdac49f9a3993
s26697 <s26697@pjwstk.edu.pl>
2025-05-08 11:08:01 +0200

    x

---
+++ 2025py_s26697/Main.py
@@ -0,0 +1,79 @@
+import random  # Importujemy biblioteke random która umożliwia generowanie liczb pseudolosowych
+
+
+def generate_dna_sequence(length):  # metoda zwracajaca wygenerowana sekwencje ACTG
+    return ''.join(random.choices('ACGT',
+                                  k=length))  # kontatanacja napisu składająco sie z losowo wybranych liter z przedziało w ilości = lenght
+
+
+def insert_name_into_sequence(sequence,
+                              name):  # metoda zwracająca sekwencje z dodanym do niej imieniem w losowym miejscu
+    insert_pos = random.randint(0,
+                                len(sequence))  # losowanie pozycji do której wpiszemy imie w granicach długosci sekwencji
+    return sequence[:insert_pos] + name + sequence[
+                                          insert_pos:]  # dodanie imienia do sekwecji na wczesniej wylosowanej pozycji i zwrócenie nowej sekwencji
+
+
+def calculate_statistics(sequence):  # funkcja generująca statystyki dla podanej sekwencji
+    # ORGINAL
+    # filtered_seq = ''.join([nt for nt in sequence if nt in 'ACGT'])
+    # total = len(filtered_seq)
+    # stats = {
+    #    'A': round((filtered_seq.count('A') / total) * 100, 1),
+    #   'C': round((filtered_seq.count('C') / total) * 100, 1),
+    #   'G': round((filtered_seq.count('G') / total) * 100, 1),
+    #  'T': round((filtered_seq.count('T') / total) * 100, 1),
+    # }
+    #   cg = filtered_seq.count('C') + filtered_seq.count('G')
+    # at = filtered_seq.count('A') + filtered_seq.count('T')
+    # cg_at_ratio = round((cg / at) * 100, 1) if at != 0 else 0.0
+    # return stats, cg_at_ratio
+
+    # MODIFIED (Poprawka 2 usunąłem filtrowanie poniewaz zmiana kolejności wykonwywania zadań sprawia, że jest ono nie potrzebne
+    # i tylko zmniejsza wydajność obliczeniową i pamięciową oraz zmniejsza czytelnośc kodu)
+    total = len(sequence)  # dlugosc sekwencji używana do obliczania %
+    stats = {  # obliczenie zaokrąglonego % występowania każdego aminokwasu w sekwecji.
+        'A': round((sequence.count('A') / total) * 100, 1),
+        'C': round((sequence.count('C') / total) * 100, 1),
+        'G': round((sequence.count('G') / total) * 100, 1),
+        'T': round((sequence.count('T') / total) * 100, 1),
+    }
+    cg = sequence.count('C') + sequence.count('G')  # oblieczenie liczby par cg
+    at = sequence.count('A') + sequence.count('T')  # oblieczenie liczby par at
+    cg_at_ratio = round((cg / at) * 100,
+                        1) if at != 0 else 0.0  # obliczenie stosunku udziału par cg do par ag w % zaokrąglone.
+    return stats, cg_at_ratio  # zwrócenie obliczonych wartosci przez metode.
+
+
+def main():
+    length = int(input("Podaj długość sekwencji: "))  # użytkownik podaje długosc sekwencji
+    seq_id = input("Podaj ID sekwencji: ")  # użytkownik podaje id sekwencji
+    description = input("Podaj opis sekwencji: ")  # użytkownik podaje opis sekwencji
+    name = input("Podaj imię: ")  # użytkownik podaje imie do wklejenia
+
+    #   ORIGINAL:
+    #   dna_sequence = generate_dna_sequence(length)
+    #  dna_with_name = insert_name_into_sequence(dna_sequence, name)
+    #   stats, cg_at_ratio = calculate_statistics(dna_with_name)
+
+    # MODIFIED (POPRAWKA NR 1, ze względu na kolejność wykonowyania zadań lub brak walidacji,
+    # podczas obliczania statystyk imię może mieć wpływ na wynik. ta poprawka ma temu zapobiec)
+    dna_sequence = generate_dna_sequence(length)
+    stats, cg_at_ratio = calculate_statistics(dna_sequence)  # zamiana kolejnosci
+    dna_with_name = insert_name_into_sequence(dna_sequence, name)
+
+    fasta_filename = f"{seq_id}.fasta"
+    with open(fasta_filename,
+              'w') as fasta_file:  # utworzenie pliku z przełącznikiem "write" o nazwie poprzedzonej id sekwencji
+        fasta_file.write(f">{seq_id} {description}\n")  # wpisanie do 1 linijki opisu w formacie fasta
+        fasta_file.write(dna_with_name + '\n')  # wipisanie do 2 linijki sekwencji dna wraz z wklejonym imieniem
+
+    print(f"\nSekwencja została zapisana do pliku {fasta_filename}")  # wypisanie na konsoli info o zapisaniu do pliku
+    print("Statystyki sekwencji:")  # wypisanie na konsoli informacji poprzedzającej wypisanie poszczególnych statystyk
+    for nt in 'ACGT':  # dla kazdego nt z populacji 'actg' ->
+        print(f"{nt}: {stats[nt]}%")  # wypisz statystyki ze wczesniej obliczonej zmiennej
+    print(f"%CG: {cg_at_ratio}")  # wypisz cg ratio
+
+
+if __name__ == "__main__":
+    main()
